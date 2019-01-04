69b724ff2c2a656da73c5ad33dbbd8a1c22d20d4
Troy Comi <troycomi@gmail.com>
2019-01-04 08:27:05 -0500

    Steps 1 and 4 Working
    -Added in generation of admixed simulations along with null
    -Completed the workflow of first shell script
    -Due to the short nature of jobs, changed submission to a single srun to
    perform simulations and initial processing

--- snakefiles/run_della.sh
+++ snakefiles/run_della.sh
@@ -3,6 +3,9 @@
 module load anaconda3
 conda activate msprime_scripts
 
+srun --ntasks=1 --cpus-per-task=10 --time=01:10:00 --mem=1G snakemake --use-conda -j 10
+
+exit
 snakemake --cluster-config 'della_cluster.yaml' \
     --cluster "sbatch --cpus-per-task={cluster.n} \
                 --mem={cluster.memory} --time={cluster.time} \
