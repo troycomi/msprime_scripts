2c8f84ba03ba6433221a4b49d37df1a84edbafe4
Troy Comi <troycomi@gmail.com>
2019-01-22 14:07:24 -0500

    Sbatch Usage
    Changed cluster execution to use sbatch instead of srun.  Also:
    -removed prefix from conda environments
    -added local rules for quick jobs
    -grouped several rules for improved slurm job usage
    -added conditional execution of sstar bin
    -removed need to produce bed files for most usage
    -added reportseff helper script

--- snakefiles/run_della.sh
+++ snakefiles/run_della.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#module load anaconda3
+module load anaconda3
 conda activate msprime_scripts
 
 set -euo pipefail
 
-##to view dependency graph (use only a few seeds!)
+## to view dependency graph (use only a few seeds!)
 #snakemake --dag | dot -Tsvg > dag.svg
 
-##unlock if job was interrupted
+## unlock if job was interrupted
 #snakemake --unlock
 
-##perform workflow.  
+## perform workflow with sbatch
 snakemake --cluster-config della_cluster.yaml \
     --cluster "sbatch --cpus-per-task={cluster.n} \
         --mem={cluster.memory} --time={cluster.time} \
@@ -20,4 +20,6 @@ snakemake --cluster-config della_cluster.yaml \
     --use-conda \
     -pr \
     -w 60 -j 50 \
+    --configfile myconfig.yaml
+    #-R $(snakemake --list-input-changes) \
     #--rerun-incomplete
